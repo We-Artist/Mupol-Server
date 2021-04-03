b827ad3e747de636b803ae0e6a4fd00ce072e143
bengHak <qkwl4678@naver.com>
2021-04-04 02:40:04 +0900

    init

---
+++ src/main/java/com/mupol/mupolserver/MupolServerApplication.java
@@ -0,0 +1,13 @@
+package com.mupol.mupolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MupolServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MupolServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mupol/mupolserver/MupolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mupol.mupolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MupolServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
