0191e507ef5e2f0e91b80b1de843171e0111e2ac
bengHak <qkwl4678@naver.com>
2021-05-01 22:57:48 +0900

    feat : add instruments

--- src/main/java/com/mupol/mupolserver/domain/instrument/Instrument.java
+++ src/main/java/com/mupol/mupolserver/domain/instrument/Instrument.java
@@ -6,10 +6,34 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Instrument {
-    piano("피아노","piano", ""),
-    piccolo("피콜로","piccolo", ""),
+    // percussion
     drum("드럼","drum", ""),
-    guitar("기타", "guitar", "")
+    marimba("마림바","marimba", ""),
+    xylophone("드럼","xylophone", ""),
+
+    // wind
+    clarinet("클라리넷","clarinet",""),
+    trumpet("트럼펫","trumpet",""),
+    flute("플룻", "flute", ""),
+    piccolo("피콜로","piccolo", ""),
+    oboe("오보에", "oboe", ""),
+    recorder("리코더", "recorder", ""),
+
+    // string
+    guitar("기타", "guitar", ""),
+    acoustic_guitar("어쿠스틱 기타", "acoustic guitar", ""),
+    base_guitar("베이스 기타", "base guitar", ""),
+    classic_guitar("클래식 기타", "classic guitar", ""),
+    electric_guitar("일렉 기타", "electric guitar", ""),
+    cello("첼로", "cello",""),
+    contrabass("콘트라베이스", "contrabass", ""),
+    piano("피아노","piano", ""),
+    ukulele("우쿨렐레", "ukulele", ""),
+    viola("비올라", "viola", ""),
+    violin("바이올린", "violin",""),
+
+    // electric
+    keyboard("키보드", "keyboard","")
     ;
 
 
