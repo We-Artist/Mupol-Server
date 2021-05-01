399171a6c8f5bc8289f9bc48dc1ce299b9ffdbfa
bengHak <qkwl4678@naver.com>
2021-05-01 22:56:41 +0900

    fix : Edit nickname validation request format

---
+++ src/main/java/com/mupol/mupolserver/dto/user/NicknameValidateReqDto.java
@@ -0,0 +1,10 @@
+package com.mupol.mupolserver.dto.user;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class NicknameValidateReqDto {
+    private String username;
+}
