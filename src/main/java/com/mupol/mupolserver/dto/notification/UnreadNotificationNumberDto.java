8f92a9fccba87d9ff38005466e6b14d657aaaf20
bengHak <qkwl4678@naver.com>
2021-06-08 10:20:45 +0900

    fix : dto getter

--- src/main/java/com/mupol/mupolserver/dto/notification/UnreadNotificationNumberDto.java
+++ src/main/java/com/mupol/mupolserver/dto/notification/UnreadNotificationNumberDto.java
@@ -1,9 +1,11 @@
 package com.mupol.mupolserver.dto.notification;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
 @Builder
+@Getter
 @ToString
 public class UnreadNotificationNumberDto {
     private int number;
